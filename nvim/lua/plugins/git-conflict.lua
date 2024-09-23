457690d76e50e7c4e9af6320b47cc4a4f3524da6
Simon Vinding Brodersen <simon@3450.dk>
2024-09-23 10:44:32 +0200

    Update to plugins neovim

--- nvim/lua/plugins/git-conflict.lua
+++ nvim/lua/plugins/git-conflict.lua
@@ -1,1 +1,15 @@
-return { 'akinsho/git-conflict.nvim', version = '*', config = true }
+return {
+  "akinsho/git-conflict.nvim",
+  version = "*",
+  config = true,
+  opts = {
+    default_mappings = {
+      ours = "co",
+      theirs = "ct",
+      none = "c0",
+      both = "cb",
+      next = "]x",
+      prev = "[x",
+    },
+  },
+}
--- nvim/lua/plugins/markdown-preview.lua
+++ nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,13 @@
-return -- install without yarn or npm
-{
-  'iamcco/markdown-preview.nvim',
-  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-  ft = { 'markdown' },
-  build = function()
-    vim.fn['mkdp#util#install']()
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+  end,
+  ft = { "markdown" },
+  config = function()
+    vim.g.mkdp_auto_close = 0
+    vim.g.mkdp_auto_start = 1
   end,
 }
