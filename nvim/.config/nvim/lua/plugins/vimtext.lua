fcef58ed8ac227ef672eba72790284b62bb70620
Simon Vinding Brodersen <simon@3450.dk>
2025-04-23 16:43:22 +0200

    Change background color

--- nvim/.config/nvim/lua/plugins/vimtext.lua
+++ nvim/.config/nvim/lua/plugins/vimtext.lua
@@ -5,6 +5,7 @@ return {
     vim.g.vimtex_syntax_conceal_disable = true
     vim.g.vimtex_mappings_disable = { ["n"] = { "K" } }
     vim.g.vimtex_compiler_progname = "lualatex"
+    vim.g.vimtex_view_method = "zathura"
   end,
   keys = {
     { "<localLeader>l", "", desc = "+vimtext" },
