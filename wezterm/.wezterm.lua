67632a916151ce1903ef197ba54c70ec36ef2421
Simon Vinding Brodersen <simon@3450.dk>
2025-06-30 11:27:19 +0200

    enable_wayland=false to fix nvidia issue

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,9 +4,8 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
+config.enable_wayland = false
 
--- For example, changing the color scheme:
 config.color_scheme = "GruvboxDarkHard"
 config.colors = {
 	background = "#141414",
@@ -19,8 +18,7 @@ config.bold_brightens_ansi_colors = "No"
 config.font_size = 12
 config.enable_tab_bar = false
 config.window_close_confirmation = "NeverPrompt"
--- and finally, return the configuration to wezterm
---
+
 config.term = "wezterm"
 config.enable_kitty_graphics = true
 config.max_fps = 120
@@ -32,7 +30,5 @@ config.keys = {
 		action = wezterm.action.ToggleFullScreen,
 	},
 }
-
-config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
 return config
