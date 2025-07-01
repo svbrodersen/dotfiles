787c0bba0dba196f76e8c159610acf65aea11bd9
Simon Vinding Brodersen <simon@3450.dk>
2025-07-01 16:34:51 +0200

    Setup more lsps

--- nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
+++ nvim/.config/nvim/lua/kickstart/plugins/lspconfig.lua
@@ -175,6 +175,9 @@ return {
         },
         pyright = {},
         rust_analyzer = {},
+        clangd = {},
+        neocmake = {},
+        jdtls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
