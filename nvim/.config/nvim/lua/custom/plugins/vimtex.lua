4434da5c1edffedb39faf418ad0e69e6be69e00a
Simon Vinding Brodersen <simon@3450.dk>
2025-07-01 16:09:31 +0200

    change to kickstart nvim

---
+++ nvim/.config/nvim/lua/custom/plugins/vimtex.lua
@@ -0,0 +1,9 @@
+return {
+  'lervag/vimtex',
+  lazy = false, -- lazy-loading will disable inverse search
+  config = function()
+    vim.g.vimtex_syntax_conceal_disable = true
+    vim.g.vimtex_compiler_progname = 'lualatex'
+    vim.g.vimtex_view_method = 'zathura'
+  end,
+}
