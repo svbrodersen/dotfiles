3fc066b05a546be0e2d06d0b834444ab6ed2579c
Simon Vinding Brodersen <simon@3450.dk>
2025-07-10 13:14:58 +0200

    Update to wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -14,6 +14,7 @@ config.font = wezterm.font_with_fallback({
 	"JetBrains Mono",
 	"Fira Code",
 })
+config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.bold_brightens_ansi_colors = "No"
 config.font_size = 12
 config.enable_tab_bar = false
