3acf7e90dc6f2c477a60e90eb057cc93301d506e
Simon Vinding Brodersen <simon@3450.dk>
2025-09-06 10:11:50 +0200

    Change latex-snippets

--- nvim/.config/nvim/lua/custom/plugins/latex_snippets.lua
+++ nvim/.config/nvim/lua/custom/plugins/latex_snippets.lua
@@ -1,4 +1,14 @@
 return {
-  'evesdropper/luasnip-latex-snippets.nvim',
-  -- vimtex isn't required if using treesitter
+  'iurimateus/luasnip-latex-snippets.nvim',
+  dependencies = { 'L3MON4D3/LuaSnip' },
+  config = function()
+    local luasnip = require 'luasnip'
+    luasnip.config.setup { enable_autosnippets = true }
+
+    -- Load the latex snippets
+    require('luasnip-latex-snippets').setup {
+      use_treesitter = true,
+      allow_on_markdown = true,
+    }
+  end,
 }
