067c2e1d173750ccd2dd71bb2e0a1bc189f2a9ce
Simon Vinding Brodersen <simon@3450.dk>
2025-10-27 08:24:28 +0100

    add vscode to luasnip

--- nvim/.config/nvim/lua/custom/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/custom/plugins/luasnip.lua
@@ -1,7 +1,20 @@
 return {
   'L3MON4D3/LuaSnip',
+  dependencies = {
+    {
+      "rafamadriz/friendly-snippets",
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+        require("luasnip.loaders.from_vscode").lazy_load({ paths = { vim.fn.stdpath("config") .. "/snippets" } })
+      end,
+    },
+  },
+  opts = {
+    history = true,
+    delete_check_events = "TextChanged",
+  },
   config = function()
-    require('luasnip.loaders.from_snipmate').load { paths = '~/.config/nvim/snippets/' }
-    require('luasnip.loaders.from_lua').lazy_load { paths = '~/.config/nvim/snippets/' }
+    require('luasnip.loaders.from_snipmate').load { paths = vim.fn.stdpath("config") .. "/snippets" }
+    require('luasnip.loaders.from_lua').lazy_load { paths = vim.fn.stdpath("config") .. "/snippets" }
   end,
 }
