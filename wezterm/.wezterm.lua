54d7af28f3555f240a3e4bddc37dfdc50ac798f5
Simon Vinding Brodersen <simon@3450.dk>
2026-03-14 12:24:56 +0100

    update to wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,30 +4,16 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.enable_wayland = false
-
 config.color_scheme = 'Gruvbox Material (Gogh)'
 config.colors = {
 	background = "#111111",
 }
-config.font = wezterm.font {
-  family = 'JetBrains Mono',
-  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-}
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
-config.bold_brightens_ansi_colors = "No"
 config.enable_tab_bar = true
 config.hide_tab_bar_if_only_one_tab = true
 config.window_close_confirmation = "NeverPrompt"
 config.enable_kitty_graphics = true
 config.max_fps = 120
-
-config.window_padding = {
-  left = 0,
-  right = 0,
-  top = 0,
-  bottom = 0,
-}
 
 config.keys = {
   -- Equivalent to `close_tab:this`
