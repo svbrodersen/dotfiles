68d50c76d9e8d18536a21427e6ef42f6dbe991bb
Simon Vinding Brodersen <simon@3450.dk>
2026-05-14 09:45:57 +0200

    fix blink url

--- nvim/.config/nvim/plugin/blink.lua
+++ nvim/.config/nvim/plugin/blink.lua
@@ -1,4 +1,4 @@
-vim.pack.add({ 'saghen/blink.lib', 'saghen/blink.cmp' })
+vim.pack.add({ 'https://github.com/saghen/blink.lib.git', 'https://github.com/saghen/blink.cmp.git' })
 local cmp = require('blink.cmp')
 cmp.build():wait(60000)
 cmp.setup()
