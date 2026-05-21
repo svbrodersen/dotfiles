61668cb1e4390330124abcd9144f47c87a195c41
Simon Vinding Brodersen <simon@3450.dk>
2026-05-21 20:48:58 +0200

    fish config update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -70,10 +70,9 @@ if status is-interactive
     #     string join '' -- (whoami) (set_color green) '@' (set_color red) (prompt_hostname) (set_color green) ":" (prompt_pwd) (set_color normal)  (fish_git_prompt)  \n $stat '> '
     # end
 
-    set --global tide_pwd_color_anchors "normal"
-    set --global tide_pwd_color_dirs "normal"
-    set --global tide_pwd_color_truncated_dirs "normal"
-    set --global tide_git_color_branch "purple"
+    set -gx tide_pwd_color_anchors brwhite  # Bright White for important/current folder
+    set -gx tide_pwd_color_dirs white       # Regular White for parent folders
+    set -gx tide_git_color_branch purple
 end
 
 if status --is-login
